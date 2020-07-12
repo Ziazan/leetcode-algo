a2043e133e45f64c2abedf6c652dd669fc0f8a68
ziazan <494074947@qq.com>
2020-07-12 19:31:33 +0800

    141环形链表

--- 141.环形链表.js
+++ 141.环形链表.js
@@ -1,4 +1,12 @@
 /*
+ * @Author: your name
+ * @Date: 2020-07-12 18:46:55
+ * @LastEditTime: 2020-07-12 19:27:02
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /leetcode/141.环形链表.js
+ */ 
+/*
  * @lc app=leetcode.cn id=141 lang=javascript
  *
  * [141] 环形链表
@@ -18,7 +26,19 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    
+    let slow = fast = head;
+    while(fast && fast.next){
+        if(fast.next === slow){
+            return true
+         }
+        fast = fast.next.next;
+        slow = slow.next
+    }
+    return false
 };
+//[3,2,0,-4]\n-1
+//[3,2,0,-4]\n0
+//[1]\n0
+//[1,2]\n0
 // @lc code=end
 
