effbd6336fbb6c753da4ce3ccf19f51a5fd5f14a
ziazan <494074947@qq.com>
2020-10-03 12:25:25 +0800

    1370. 上升下降字符串 --桶计数法

--- 1370.上升下降字符串.js
+++ 1370.上升下降字符串.js
@@ -10,35 +10,31 @@
  * @return {string}
  */
 var sortString = function (s) {
-    //排序
-    //最小，不等 存入，删除
-    let sArr = s.split('').sort((a, b) => a.charCodeAt() - b.charCodeAt())
-    let newStr = "";
-    console.log(sArr)
-    while (sArr.length) {
-        //正
-        newStr += sArr[0];
-        sArr.splice(0, 1)
-        for (let i = 0; i < sArr.length; i++) {
-            let last = newStr[newStr.length - 1]//字符串最后一个字母
-            if (sArr[i] !== last && sArr[i].charCodeAt() > last.charCodeAt()) {
-                newStr += sArr.splice(i, 1).join('');
-                i = i - 1
-            }
-        }
-        //逆
-        newStr += sArr[sArr.length - 1] || "";
-        sArr.splice(sArr.length - 1, 1)
-        for (let i = sArr.length - 1; i >= 0; i--) {
-            let last = newStr[newStr.length - 1]
-            if (sArr[i] !== last && sArr[i].charCodeAt() < last.charCodeAt()) {
-                newStr += sArr.splice(i, 1).join('');
-            }
-        }
+    let bucket = new Array(26).fill(0);
 
+    let str = "";
+    for (let i = 0; i < s.length; i++) {
+        bucket[s[i].charCodeAt() - 97]++
     }
-    return newStr
-};
+    let len = s.length;
+    while (len) {
+        for (let i = 0; i < 26; i++) {
+            if (bucket[i]) {
+                str += String.fromCharCode(i + 97)
+                bucket[i]--
+                len--
+            }
+        }
+        for (let i = 25; i >= 0; i--) {
+            if (bucket[i]) {
+                str += String.fromCharCode(i + 97)
+                bucket[i]--
+                len--
+            }
+        }
+    }
 
+    return str
+};
 // @lc code=end
 
