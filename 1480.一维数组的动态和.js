8261554ec585a436c03a8644078f62219402e497
ziazan <494074947@qq.com>
2020-10-08 18:15:47 +0800

    1480.一维数组的动态和.js 递归

--- 1480.一维数组的动态和.js
+++ 1480.一维数组的动态和.js
@@ -12,14 +12,21 @@
 var runningSum = function (nums) {
     if (!nums || nums.length < 2) return nums
     let newArr = []
-    nums.reduce((pre, next, index) => {
-        newArr.push(pre + next);
-        return pre + next
-    }, 0)
+    sum(newArr, nums, nums.length - 1)
+    console.log(newArr)
     return newArr;
 
 };
 runningSum([3, 1, 2, 10, 1])
 
+function sum(newArr, nums, index) {
+    if (index === 0) {
+        newArr[0] = nums[0]
+        return newArr[0]
+    } else {
+        newArr[index] = nums[index] + sum(newArr, nums, index - 1)
+        return newArr[index]
+    }
+}
 // @lc code=end
 
